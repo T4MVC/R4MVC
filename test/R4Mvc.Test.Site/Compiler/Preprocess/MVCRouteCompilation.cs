ebc7c043d7e4742669211567c25dde8a98807a2e
wwwlicious <773dcf03@opayq.com>
2015-01-29 23:53:12 +0000

    changing where files are to enable test project to fire off Compilermodule rather than have it execute on the t4mvc project. Imagine that the MVCRouteCompilation class would still be in T4MVC project and copied from nuget package into target project but will leave in test project for now

---
+++ test/R4Mvc.Test.Site/Compiler/Preprocess/MVCRouteCompilation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace R4Mvc.Compiler.Preprocess
+{
+	public class MVCRouteCompilation : R4Mvc.Compiler.Preprocess.MVCRouteCompilerModule
+	{
+		public MVCRouteCompilation(IServiceProvider provider) : base(provider)
+		{
+		}
+	}
+}
