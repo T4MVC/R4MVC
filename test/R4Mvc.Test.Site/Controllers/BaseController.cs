c0ec5f9e2c534f9d93812b8a2e24c0470e7c657e
wwwlicious <773dcf03@opayq.com>
2015-02-01 16:59:56 +0000

    Adding some generic based controllers and existing partial for testing

--- test/R4Mvc.Test.Site/Controllers/BaseController.cs
+++ test/R4Mvc.Test.Site/Controllers/BaseController.cs
@@ -1,20 +1,50 @@
-﻿// BaseController.cs
-/// <summary>
+﻿/// <summary>
 /// Tests SyntaxRewriters ability to find controllers
 /// </summary>
 namespace R4Mvc.Test.Site.Controllers
 {
+	using System.CodeDom.Compiler;
+
 	using Microsoft.AspNet.Mvc;
 
 	public abstract class BaseController : Controller
 	{
+		ActionResult TestMethod()
+		{
+			return new EmptyResult();;
+		}
+
+		ActionResult TestMethod(string arg1)
+		{
+			return new EmptyResult();
+		}
 	}
 
-	public class CustomerController1 : BaseController
+	public abstract class BaseController<T> : BaseController
+		where T : class
 	{
 	}
 
-	public class CustomController2 : CustomerController1
+	public class CustomGenericController : BaseController<Dummy>
 	{
 	}
+
+	public class CustomController1 : BaseController
+	{
+	}
+
+	public class CustomController2 : CustomController1
+	{
+	}
+
+	public partial class CustomPartialController : BaseController
+	{
+	}
+
+	[GeneratedCode("R4MVC", "1.0.0.0")]
+	public partial class CustomPartialController
+	{
+	}
+
+	public partial class Dummy {}
 }
