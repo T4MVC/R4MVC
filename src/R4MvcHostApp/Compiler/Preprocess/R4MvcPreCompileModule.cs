1836d0093a8802e39dcd8db4d9a2b6c7a1dee242
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-02-05 15:28:17 +0000

    Add stub pre-compile module

---
+++ src/R4MvcHostApp/Compiler/Preprocess/R4MvcPreCompileModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Framework.Runtime;
+
+namespace R4MvcHostApp.Compiler.Preprocess
+{
+    public class R4MvcPreCompileModule : ICompileModule
+    {
+        public void BeforeCompile(IBeforeCompileContext context)
+        {
+               
+        }
+
+        public void AfterCompile(IAfterCompileContext context)
+        {
+
+        }
+    }
+}
