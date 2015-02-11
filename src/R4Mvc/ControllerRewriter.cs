6900f3757071604d742fa6f87db5b5f9ebbce071
wwwlicious <773dcf03@opayq.com>
2015-02-11 21:57:58 +0000

    Cleaning up and improving some naming

--- src/R4Mvc/ControllerRewriter.cs
+++ src/R4Mvc/ControllerRewriter.cs
@@ -14,31 +14,31 @@ namespace R4Mvc
 	/// </summary>
 	public class ControllerRewriter : CSharpSyntaxRewriter
 	{
-		private readonly SemanticModel _model;
+		private readonly CSharpCompilation _compiler;
 
 		private readonly List<ClassDeclarationSyntax> _mvcControllerClassNodes = new List<ClassDeclarationSyntax>();
 
-		public ControllerRewriter(SemanticModel model)
+		public ControllerRewriter(CSharpCompilation compiler)
 		{
-			_model = model;
+			this._compiler = compiler;
 		}
 
 		public ClassDeclarationSyntax[] MvcControllerClassNodes => this._mvcControllerClassNodes.ToArray();
 
 		public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
 		{
-			var symbol = _model.GetDeclaredSymbol(node);
+			var symbol = _compiler.GetSemanticModel(node.SyntaxTree).GetDeclaredSymbol(node);
 			if (symbol.InheritsFrom<Controller>())
 			{
 				_mvcControllerClassNodes.Add(node);
 
-				// if controller is not partial, make it so and create new partial class/file and return
+				// if controller is not partial, make it so
 				Debug.WriteLine("{0} inherits from {1}", symbol.ToString(), typeof(Controller).FullName);
 				if (!node.Modifiers.Any(SyntaxKind.PartialKeyword))
 				{
 					// Mark class partial
 					Debug.WriteLine("Marking {0} class a partial", symbol);
-					node = R4MvcHelpers.AddPartialModifier(node);
+					node = node.WithPartialModifier();
 				}
 			}
 
@@ -47,28 +47,14 @@ namespace R4Mvc
 
 		public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
 		{
-			// first, if !controller class return
-			//var symbol = _model.GetDeclaredSymbol(node);
-			//if (symbol.InheritsFrom<Controller>() && symbol.IsVirtual)
+			// symbol wont be found if we've modified class
+			// TODO how to update out of sync node?
+			//var model = _compiler.GetSemanticModel(node.SyntaxTree);
+			//var symbol = model.GetDeclaredSymbol(node);
+			//if (symbol.InheritsFrom<Controller>() && !symbol.IsVirtual)
 			//{
-			//	// NOTE Cannot use original semantic model after node has been modified
-			//	//var symbol = _model.GetDeclaredSymbol(node);
-			//	//Debug.WriteLine("visiting controller method {0} from {1}", symbol.ToString(), node.SyntaxTree.FilePath);
-
-			//	if (node.Modifiers.Any(x => x.IsKind(SyntaxKind.PublicKeyword)))
-			//	{
-			//		// only public methods
-
-			//		// generated class = true
-			//		// is method virtual
-			//		//    if true, find corresponding method name + arg count in controller class
-			//		//    if false, remove method
-
-			//		// generated class = false
-			//		// is method virtual
-			//		//   if true, return
-			//		//   if false, mark as virtual
-			//	}
+			//	Debug.WriteLine("visiting controller method {0} from {1}", symbol.ToString(), node.SyntaxTree.FilePath);
+			//	node.WithVirtualModifier();
 			//}
 
 			return base.VisitMethodDeclaration(node);
