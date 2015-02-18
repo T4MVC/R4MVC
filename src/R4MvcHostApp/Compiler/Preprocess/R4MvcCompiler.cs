8f0b125b9825252d69eff0034909377b7dbc0db8
wwwlicious <773dcf03@opayq.com>
2015-02-18 20:36:47 +0000

    Adding RazorViewLocator and loading using dependencyinjection with default fallback adding viewnames to generated class

--- src/R4MvcHostApp/Compiler/Preprocess/R4MvcCompiler.cs
+++ src/R4MvcHostApp/Compiler/Preprocess/R4MvcCompiler.cs
@@ -1,4 +1,4 @@
-using System.IO;
+using System;
 
 using R4Mvc;
 
@@ -6,7 +6,7 @@ namespace R4MvcHostApp.Compiler.Preprocess
 {
 	public class R4MvcCompiler : R4MVCCompilerModule
 	{
-		public R4MvcCompiler()
+		public R4MvcCompiler(IServiceProvider serviceProvider) : base(serviceProvider)
 		{
 			// You can override the generated file location here
 			// this.SetGeneratedFilePath(() => Path.Combine(this.Project.ProjectDirectory, "R4Custom.cs"));
