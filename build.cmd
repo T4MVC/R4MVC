33da3f3e9240d52f33ec8e5c2c204f4f250d162a
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-06-19 08:22:56 +0100

    Upgrade xunit to 2.1.0-beta2 and switch over to the dnx runner

--- build.cmd
+++ build.cmd
@@ -42,7 +42,7 @@ CALL dnu build test\R4Mvc.Test || set errorlevel=1
 IF NOT EXIST test-results MD test-results
 
 cd test\R4Mvc.Test
-CALL dnx test -xml ..\..\test-results\R4Mvc.Test.clr.xml || set errorlevel=1
+CALL dnx . test -xml ..\..\test-results\R4Mvc.Test.clr.xml || set errorlevel=1
 
 REM CALL %USERPROFILE%\.dnx\bin\dnvm use default -r coreclr || set errorlevel=1
 REM CALL dnx test -xml ..\..\test-results\R4Mvc.Test.coreclr.xml || set errorlevel=1
