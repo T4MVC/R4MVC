dd797d71b4ddd31b538e8c68548e56312c8046d1
Artiom Chilaru <artiom@flexlabs.org>
2017-06-02 11:04:03 +0100

    ! Patching the build script?

--- build.cmd
+++ build.cmd
@@ -2,8 +2,12 @@ dotnet --info
 dotnet restore -v Minimal
 
 dotnet build "src\R4Mvc"
-dotnet build "src\R4MvcHostApp"
+dotnet build "src\R4Mvc.Tools"
 dotnet build "src\R4Mvc.Test"
 
 cd test\R4Mvc.Test
 dotnet test
+
+cd ..\..
+dotnet pack "src\R4Mvc"
+dotnet pack "src\R4Mvc.Tools"
