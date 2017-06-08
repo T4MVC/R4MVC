f7d2f4ad0813e489fd13af63c31f66b111caff78
Artiom Chilaru <artiom@flexlabs.org>
2017-06-08 07:50:03 +0100

    + Build definition scripts + Build suffix versioning

--- build.cmd
+++ build.cmd
@@ -1,13 +1,7 @@
 dotnet --info
 dotnet restore -v Minimal
 
-dotnet build "src\R4Mvc"
-dotnet build "src\R4Mvc.Tools"
-dotnet build "src\R4Mvc.Test"
+dotnet build -c Release
 
 cd test\R4Mvc.Test
 dotnet test
-
-cd ..\..
-dotnet pack "src\R4Mvc"
-dotnet pack "src\R4Mvc.Tools"
