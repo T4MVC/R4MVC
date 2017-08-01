1364f88851e868caaa1e5fef2ec2f92cab074b2d
Valerio Borioni <valeriob@gmail.com>
2017-08-01 09:05:32 +0200

    Upgrade to netstandard2.0 and aspnetcore 2.0
    - R4MVC upgraded to netstandard2.0
    - R4MvcHostApp upgraded aspnetcore 2.0

--- src/R4MvcHostApp/Controllers/HomeController.cs
+++ src/R4MvcHostApp/Controllers/HomeController.cs
@@ -1,35 +1,37 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using R4MvcHostApp.Models;
 
 namespace R4MvcHostApp.Controllers
 {
-    public partial class HomeController : Controller
+    public class HomeController : Controller
     {
-        public virtual IActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public virtual IActionResult About()
+        public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public virtual IActionResult Contact()
+        public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public virtual IActionResult Error()
+        public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
