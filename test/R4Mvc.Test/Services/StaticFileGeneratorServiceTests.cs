a0d49247d445468d90513f653b2812180d27c042
Artiom Chilaru <artiom@flexlabs.org>
2017-12-08 18:25:07 +0000

    ! Updating test

--- test/R4Mvc.Test/Services/StaticFileGeneratorServiceTests.cs
+++ test/R4Mvc.Test/Services/StaticFileGeneratorServiceTests.cs
@@ -8,24 +8,6 @@ namespace R4Mvc.Test.Services
 {
     public class StaticFileGeneratorServiceTests
     {
-        [Theory]
-        [InlineData("helloworld", "helloworld")]
-        [InlineData("hello-world", "hello_world")]
-        [InlineData("hello world", "hello_world")]
-        [InlineData("hello%world", "hello_world")]
-        [InlineData("hello % world", "hello___world")]
-        [InlineData("helloworld^", "helloworld_")]
-        [InlineData("helloworld^^", "helloworld__")]
-        [InlineData("^helloworld", "_helloworld")]
-        [InlineData("^^helloworld", "__helloworld")]
-        [InlineData("helloworld0", "helloworld0")]
-        [InlineData("hello0world", "hello0world")]
-        [InlineData("0helloworld", "_0helloworld")]
-        public void SanitiseName(string name, string sanitisedName)
-        {
-            Assert.Equal(sanitisedName, StaticFileGeneratorService.SanitiseName(name));
-        }
-
         [Fact]
         public void CreateLinks()
         {
