d4a63be8b1839e784ee6d6c5e3890ae95c29e07b
Valerio Borioni <valeriob@gmail.com>
2017-12-19 17:53:47 +0100

    settings wwwrootInclude parameter

--- src/R4Mvc.Tools/Services/StaticFileGeneratorService.cs
+++ src/R4Mvc.Tools/Services/StaticFileGeneratorService.cs
@@ -24,7 +24,8 @@ namespace R4Mvc.Tools.Services
         public MemberDeclarationSyntax GenerateStaticFiles(string projectRoot)
         {
             var staticFilesRoot = GetStaticFilesPath(projectRoot);
-            var staticfiles = _staticFileLocators.SelectMany(x => x.Find(staticFilesRoot));
+            var staticfiles = _staticFileLocators.SelectMany(x => x.Find(staticFilesRoot))
+                .Where(r=> _settings.IncludeContainer(r.Container));
 
             var linksClass = SyntaxFactory.ClassDeclaration(_settings.LinksNamespace)
                 .WithModifiers(SyntaxKind.PublicKeyword, SyntaxKind.StaticKeyword, SyntaxKind.PartialKeyword)
--- src/R4Mvc.Tools/Settings.cs
+++ src/R4Mvc.Tools/Settings.cs
@@ -1,4 +1,6 @@
-﻿namespace R4Mvc.Tools
+﻿using System.Linq;
+
+namespace R4Mvc.Tools
 {
     public class Settings
     {
@@ -7,5 +9,17 @@
         public string LinksNamespace { get; set; } = "Links";
         public bool SplitIntoMultipleFiles { get; set; } = true;
         public string wwwroot { get; set; } = "wwwroot";
+        public string[] wwwrootInclude { get; set; }
+
+        internal bool IncludeContainer(string container)
+        {
+            return wwwrootInclude == null || wwwrootInclude.Any(i => Match(i, container));
+        }
+
+        bool Match(string current, string container)
+        {
+            return container.StartsWith(current, System.StringComparison.InvariantCultureIgnoreCase);
+        }
+
     }
 }
