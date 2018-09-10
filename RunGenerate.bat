455ef3948b4d23477e76e3834e77df39689f9fde
Artiom Chilaru <artiom@flexlabs.org>
2018-09-10 09:25:55 +0100

    Running the generator with the correct configuration

--- RunGenerate.bat
+++ RunGenerate.bat
@@ -1,2 +1,7 @@
-.\src\R4Mvc.Tools\bin\Debug\net462\dotnet-r4mvc.exe generate .\samples\AspNetSimple\AspNetSimple.csproj
-.\src\R4Mvc.Tools\bin\Debug\net462\dotnet-r4mvc.exe generate .\samples\AspNetFeatureFolders\AspNetFeatureFolders.csproj
+@ECHO OFF
+IF NOT DEFINED CONFIGURATION SET CONFIGURATION=Debug
+IF NOT "%1"=="" SET CONFIGURATION=%1
+
+@ECHO ON
+.\src\R4Mvc.Tools\bin\%CONFIGURATION%\net462\dotnet-r4mvc.exe generate .\samples\AspNetSimple\AspNetSimple.csproj
+.\src\R4Mvc.Tools\bin\%CONFIGURATION%\net462\dotnet-r4mvc.exe generate .\samples\AspNetFeatureFolders\AspNetFeatureFolders.csproj
