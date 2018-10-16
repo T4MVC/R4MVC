24116919b2c386b6cbf1281970c58cfc82fe050c
Artiom Chilaru <artiom@flexlabs.org>
2018-10-16 18:37:04 +0100

    + Adding test case for action overrides

--- samples/AspNetSimple/Controllers/DebugControllerBase.cs
+++ samples/AspNetSimple/Controllers/DebugControllerBase.cs
@@ -6,5 +6,6 @@ namespace AspNetSimple.Controllers
     public abstract class DebugControllerBase : Controller
     {
         public IActionResult Debug() => throw new NotImplementedException();
+        public virtual IActionResult OverrideMe() => throw new NotImplementedException();
     }
 }
